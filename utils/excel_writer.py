fcaab28ed38754a204d493a07e7ddda93b67abaf
CapelleGab <gabin.capelle.pro@gmail.com>
2025-05-26 16:08:17 +0200

    🐛 Correction du bug d'export Excel - Ajout du paramètre fichier_path optionnel

--- utils/excel_writer.py
+++ utils/excel_writer.py
@@ -8,15 +8,19 @@ import pandas as pd
 from config import FICHIER_EXCEL, NOMS_FEUILLES
 
 
-def sauvegarder_excel(stats_final, moyennes_equipe):
+def sauvegarder_excel(stats_final, moyennes_equipe, fichier_path=None):
     """
     Sauvegarde les données dans un fichier Excel.
     
     Args:
         stats_final (pd.DataFrame): Statistiques par employé
         moyennes_equipe (pd.DataFrame): Moyennes par équipe
+        fichier_path (str, optional): Chemin du fichier Excel. Si None, utilise FICHIER_EXCEL de config.
     """
-    # print(f"\nSauvegarde dans {FICHIER_EXCEL}...")
-    with pd.ExcelWriter(FICHIER_EXCEL, engine='openpyxl') as writer:
+    # Utiliser le chemin fourni ou celui de la config par défaut
+    excel_path = fichier_path if fichier_path is not None else FICHIER_EXCEL
+    
+    # print(f"\nSauvegarde dans {excel_path}...")
+    with pd.ExcelWriter(excel_path, engine='openpyxl') as writer:
         stats_final.to_excel(writer, sheet_name=NOMS_FEUILLES['statistiques'], index=False)
         moyennes_equipe.to_excel(writer, sheet_name=NOMS_FEUILLES['moyennes'], index=False) 
